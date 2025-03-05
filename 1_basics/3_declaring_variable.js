cf2146dc4ebf9c6bc2289c507efb2a51e4eb9e81
임성호 <tjd4119@naver.com>
2025-03-05 21:26:35 +0900

    JS 기본기 복습 1일차

---
+++ 1_basics/3_declaring_variable.js
@@ -0,0 +1,38 @@
+/**
+ * Variable 선언하기
+ * 
+ * 1) var - 더이상 쓰지 않는다.
+ * 2) let 
+ * 3) const 
+ */
+var name = 'Name';
+console.log(name)
+
+var age = 32;
+console.log(age);
+
+/**
+ * let과 var로 선언하면 
+ * 값을 추후 변경 할 수 있다.
+ */
+age = 33;
+console.log(age);
+
+const newJeans = '뉴진스'
+console.log(newJeans);
+
+// newJeans = '뉴진스2';
+
+/**
+ * 선언과 할당 
+ * 1) 변수를 선언하는 것.
+ *   - let name;
+ * 2) 할당
+ *   - let name = 'Name'
+ */
+
+let girlFriend;
+console.log(girlFriend);
+
+// const 는 값을 바꾸지 못하기 때문에 반드시 초기화를 해야한다.
+// const girlFriend2;
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # JSStudy
 JS 기본부터 공부
+
+# Node 설치
+brew install node
+
+## Node Version
+
+## Vscode Extension
+- runCode
+- Live Server
+- JavaScript(ES6) code snippets
